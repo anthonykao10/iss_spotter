1636da410b5b2a03b6189cef47926350c0881b04
anthonykao10 <anthonykao10@gmail.com>
2019-08-29 22:39:53 +0000

    Implement fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((err, ip) => {
+// fetchMyIP((err, ip) => {
+//   if (err) {
+//     console.log("It didn't work!\n" , err);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('162.245.144.188', (err, data) => {
   if (err) {
     console.log("It didn't work!\n" , err);
     return;
   }
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned coords:' , data);
 });
--- iss.js
+++ iss.js
@@ -15,8 +15,8 @@ const fetchMyIP = function(callback) {
     if (err) return callback(err, null);
     // If non-200 status, assume server error
     if (res.statusCode !== 200) {
-      const msg = `Status Code ${res.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+      const msg = `Status Code ${res.statusCode} when fetching IP. Response:\n ${body}`;
+      callback(Error(msg).toString(), null);
       return;
     }
     // On success, pass ip to callback
@@ -25,4 +25,36 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (err, res, body) => {
+    // error can be set if invalid domain, user is offline, etc.
+    if (err) return callback(err, null);
+    // if non-200 status, assume server error
+    if (res.statusCode !== 200) {
+      const msg = `Status code ${res.statusCode} when fetching coords for IP. Response:\n ${body}`;
+      callback(Error(msg).toString(), null);
+      return;
+    }
+
+    let data = JSON.parse(body).data;
+    let coords = {
+      latitude: data.latitude,
+      longitude: data.longitude
+    };
+    callback(err, coords);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
