3a3e9db5d3a8ee2135fb9256d117e8aa85c0da22
anthonykao10 <anthonykao10@gmail.com>
2019-08-29 22:09:15 +0000

    Add check for responses with non-200 status code in fetchMyIP

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ fetchMyIP((err, ip) => {
   if (err) {
     console.log("It didn't work!\n" , err);
     return;
-  } 
+  }
   console.log('It worked! Returned IP:' , ip);
 });
--- iss.js
+++ iss.js
@@ -8,15 +8,21 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org/?format=json', (err, res, body) => {
-    // On error pass err to callback
+    // Error can be set if invalid domain, user is offline, etc.
     if (err) return callback(err, null);
+    // If non-200 status, assume server error
+    if (res.statusCode !== 200) {
+      const msg = `Status Code ${res.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
     // On success, pass ip to callback
     const ip = JSON.parse(body).ip;
     callback(null, ip);
-  })
+  });
 };
 
 module.exports = { fetchMyIP };
